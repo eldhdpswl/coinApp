f6e1317930c7315d6d0faa0f87cd667dfb1c82af
신동금 <eldhdpswl@gmail.com>
2023-12-30 21:05:10 +0900

    coinApp main commit

---
+++ app/src/main/java/com/example/coco/background/GetCoinPriceRecentContractedWorkManager.kt
@@ -0,0 +1,86 @@
+package com.example.coco.background
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.coco.db.entity.SelectedCoinPriceEntity
+import com.example.coco.network.model.RecentCoinPriceList
+import com.example.coco.repository.DBRepository
+import com.example.coco.repository.NetworkRepository
+import timber.log.Timber
+import java.sql.Timestamp
+import java.util.Calendar
+import java.util.Date
+
+
+// 최근 거래된 코인 가격 내역을 가져오는 WorkManager
+
+// 1. 저희가 관심있어하는 코인 리스트를 가져와서
+// 2. 관심있는 코인 각각의 가격 변동 정보를 가져와서 (New API)
+// 3. 관심있는 코인 각각의 가격 변동 정보 DB에 저장
+
+class GetCoinPriceRecentContractedWorkManager(val context : Context, workerParameters: WorkerParameters)
+    : CoroutineWorker(context, workerParameters){
+
+    private val dbRepository = DBRepository()
+    private val netWorkRepository = NetworkRepository()
+
+    override suspend fun doWork(): Result {
+
+        Timber.d("doWork")
+
+        getAllInterestSelectedCoinData()
+
+        return Result.success()
+
+    }
+
+
+    // 1. 저희가 관심있어하는 코인 리스트를 가져와서
+    suspend fun getAllInterestSelectedCoinData() {
+        val selectedCoinList = dbRepository.getAllInterestSelectedCoinData()
+
+        val timeStamp = Calendar.getInstance().time
+
+        for(coinData in selectedCoinList) {
+//            Timber.d(coinData.toString())
+
+            // 2. 관심있는 코인 각각의 가격 변동 정보를 가져와서 (New API)
+            val recentCoinPriceList = netWorkRepository.getInterestCoinPriceData(coinData.coin_name)
+
+            Timber.d(recentCoinPriceList.toString())
+
+            saveSelectedCoinPrice(
+                coinData.coin_name,
+                recentCoinPriceList,
+                timeStamp
+            )
+
+        }
+    }
+
+    // 3. 관심있는 코인 각각의 가격 변동 정보 DB에 저장
+    fun saveSelectedCoinPrice(
+        coinName : String,
+        recentCoinPriceList: RecentCoinPriceList,
+        timeStamp : Date
+    ){
+        // data[0]인 이유는 첫번째 데이터만 가져와서 쓰겠다
+        val selectedCoinPriceEntity = SelectedCoinPriceEntity(
+            0,
+            coinName,
+            recentCoinPriceList.data[0].transaction_date,
+            recentCoinPriceList.data[0].type,
+            recentCoinPriceList.data[0].units_traded,
+            recentCoinPriceList.data[0].price,
+            recentCoinPriceList.data[0].total,
+            timeStamp
+
+        )
+
+        dbRepository.insertCoinPriceData(selectedCoinPriceEntity)
+
+    }
+
+
+}
